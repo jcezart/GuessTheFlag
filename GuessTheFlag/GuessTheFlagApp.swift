e2906302172a403de35f7040d3dea5e029bfb59e
Julio Cezar <jcezar.t@gmail.com>
2026-09-23 05:51:19 -0300

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Julio Cezar Teixeira on 15/09/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
